b6041e6b6c209edb475b98515d175f1b5016b469
John Regehr <regehr@cs.utah.edu>
2016-05-05 10:53:47 +0200

    initial checkin

---
+++ interval-bitwise.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "llvm/ADT/APInt.h"
+#include "llvm/IR/ConstantRange.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+static const int Width = 8;
+
+ConstantRange next(const ConstantRange CR) {
+  auto L = CR.getLower();
+  auto U = CR.getUpper();
+  do {
+    if (U.isMaxValue()) {
+      L++;
+      U = APInt(Width, 0);
+    } else {
+      U++;
+    }
+  } while (L == U && !L.isMinValue() && !L.isMaxValue());
+  return ConstantRange(L, U);
+}
+
+int main(void) {
+  ConstantRange C(Width, /* isFullSet= */false);
+  do {
+    outs() << C << "\n";
+    C = next(C);
+  } while (!C.isEmptySet());
+  return 0;
+}
