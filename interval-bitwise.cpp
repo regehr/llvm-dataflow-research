eb22ed33a8fe6f4b46f991f2a296a3ee0bdca49c
John Regehr <regehr@cs.utah.edu>
2016-05-05 22:33:52 +0200

    can verify CR add and subtract for small bitwidths

--- interval-bitwise.cpp
+++ interval-bitwise.cpp
@@ -5,27 +5,58 @@
 
 using namespace llvm;
 
-static const int Width = 8;
+static const int Width = 4;
+
+ConstantRange exhaustive(const ConstantRange L, const ConstantRange R) {
+  ConstantRange Res(Width, /* isFullSet= */ false);
+  if (L.isEmptySet() || R.isEmptySet())
+    return Res;
+  auto LI = L.getLower();
+  do {
+    auto RI = R.getLower();
+    do {
+      Res = Res.unionWith(ConstantRange(LI - RI));
+      RI++;
+    } while (RI != R.getUpper());
+    LI++;
+  } while (LI != L.getUpper());
+  return Res;
+}
+
+void check(ConstantRange L, ConstantRange R) {
+  ConstantRange Res1 = L.sub(R);
+  ConstantRange Res2 = exhaustive(L, R);
+  if (Res1 != Res2)
+    outs() << L << " op " << R << " = " << Res1 << " " << Res2 << "\n";
+}
 
 ConstantRange next(const ConstantRange CR) {
   auto L = CR.getLower();
   auto U = CR.getUpper();
   do {
-    if (U.isMaxValue()) {
+    if (U.isMaxValue())
       L++;
-      U = APInt(Width, 0);
-    } else {
-      U++;
-    }
+    U++;
   } while (L == U && !L.isMinValue() && !L.isMaxValue());
   return ConstantRange(L, U);
 }
 
-int main(void) {
-  ConstantRange C(Width, /* isFullSet= */ false);
+void test() {
+  ConstantRange L(Width, /* isFullSet= */ false);
+  ConstantRange R(Width, /* isFullSet= */ false);
+  long count = 0;
   do {
-    outs() << C << "\n";
-    C = next(C);
-  } while (!C.isEmptySet());
+    do {
+      check(L, R);
+      count++;
+      R = next(R);
+    } while (!R.isEmptySet());
+    L = next(L);
+  } while (!L.isEmptySet());
+  outs() << "checked " << count << " ConstantRanges\n";
+}
+
+int main(void) {
+  test();
   return 0;
 }
