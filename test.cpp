bc7b015f7bcebbf4960e4395e96595c97e122e97
John Regehr <regehr@cs.utah.edu>
2016-05-27 14:22:39 +0200

    fixes

--- test.cpp
+++ test.cpp
@@ -5,38 +5,37 @@
 
 using namespace llvm;
 
-static const int Width = 3;
+static const int Width = 4;
 
 ConstantRange exhaustive(const ConstantRange L, const ConstantRange R,
                          const ConstantRange Untrusted) {
   if (L.isEmptySet() || R.isEmptySet())
     return ConstantRange(Width, /* isFullSet= */ false);
-  bool Seen[1 << Width] = { false };
   ConstantRange Joined(Width, /* isFullSet= */ false);
   auto LI = L.getLower();
   do {
     auto RI = R.getLower();
     do {
-      auto Val = LI << RI;
+      auto Val = LI | RI;
+      if (false)
+        outs() << LI << " op " << RI << " = " << Val << "\n";
       if (!Untrusted.contains(Val)) {
         outs() << "oops!\n";
         exit(-1);
       }
-      Joined.unionWith(ConstantRange(Val));
-      Seen[Val.getLimitedValue()] = true;
+      Joined = Joined.unionWith(ConstantRange(Val));
       RI++;
     } while (RI != R.getUpper());
     LI++;
   } while (LI != L.getUpper());
-  ConstantRange Res(Width, /* isFullSet= */ false);
-  return Res;
+  return Joined;
 }
 
 void check(ConstantRange L, ConstantRange R) {
-  ConstantRange Res1 = L.shl(R);
+  ConstantRange Res1 = L.binaryOr(R);
   ConstantRange Res2 = exhaustive(L, R, Res1);
   if (Res2.getSetSize().ult(Res1.getSetSize()))
-    outs() << L << " op " << R << " = " << Res1 << " " << Res2 << "\n";
+    outs() << L << " op " << R << " =   LLVM: " << Res1 << "   precise: " << Res2 << "\n";
 }
 
 ConstantRange next(const ConstantRange CR) {
@@ -50,7 +49,7 @@ ConstantRange next(const ConstantRange CR) {
   return ConstantRange(L, U);
 }
 
-void test() {
+void testAllConstantRanges() {
   ConstantRange L(Width, /* isFullSet= */ false);
   ConstantRange R(Width, /* isFullSet= */ false);
   long count = 0;
@@ -66,6 +65,6 @@ void test() {
 }
 
 int main(void) {
-  test();
+  testAllConstantRanges();
   return 0;
 }
